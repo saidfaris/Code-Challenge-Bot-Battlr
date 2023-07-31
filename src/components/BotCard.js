f7eb37e04b5ff7e82628c140b0f70df47fab613c
sidee <said.issack@student.moringaschool.com>
2023-07-31 20:23:08 +0300

    added some functions to the BotCard.js

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,14 +1,4 @@
 import React from "react";
-
-// const botTypeClasses = {
-//   Assault: "icon military",
-//   Defender: "icon shield",
-//   Support: "icon plus circle",
-//   Medic: "icon ambulance",
-//   Witch: "icon magic",
-//   Captain: "icon star",
-// };
-
 function BotCard({ bot, handleBot, handleDeleteBot }) {
   return (
     <div className="column">
